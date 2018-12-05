f2d37f6e4d292aef733464588eff691373dd117b
tragicmuffin <33797815+tragicmuffin@users.noreply.github.com>
2018-12-05 18:14:11 -0500

    Day 5

--- day5/day5.py
+++ day5/day5.py
@@ -1,3 +1,8 @@
+## Advent of Code 2018: Day 5
+## https://adventofcode.com/2018/day/5
+## Jesse Williams
+## Answers: [Part 1]: 10804, [Part 2]: 6650
+
 import string
 
 def react(pair):
@@ -8,43 +13,41 @@ def react(pair):
         return False
 
 def applyReactions(polymer):
-      
-      i = 0
-      reactions = 0
-      # Move through polymer, applying reactions, and find the reduced chain.
-      while (i < len(polymer)-1):
-          # Pass the next unit pair in the sliding window into react
-          if (react(polymer[i:i+2])):
-              # If there was a reaction, pop these elements out of list, and back the window up to check for any chained reactions
-              polymer.pop(i)
-              polymer.pop(i)
-              i -= 1
-              reactions += 1
-              if (i < 0): i = 0  # in case we get to the start of the list and underflow
-          else:
-              # If there was no reaction, slide the window forward and continue.
-              i += 1
-              
-      
-    
+    i = 0
+    # Move through polymer, applying reactions, and find the reduced chain.
+    while (i < len(polymer)-1):
+        # Pass the next unit pair in the sliding window into react
+        if (react(polymer[i:i+2])):
+            # If there was a reaction, pop these elements out of list, and back the window up to check for any chained reactions
+            polymer.pop(i)
+            polymer.pop(i)
+            i -= 1
+            if (i < 0): i = 0  # in case we get to the start of the list and underflow
+        else:
+            # If there was no reaction, slide the window forward and continue.
+            i += 1
+    return polymer
+
+
+
 if __name__ == "__main__":
     # Capture the input unit stream as a string, then convert to list
-    with open('input.txt') as f:
+    with open('day5_input.txt') as f:
         polymerUnreacted = f.read()
     polymerUnreacted = list(polymerUnreacted)[:-1]  # cut off the \n element
     polymerUnreactedLength = len(polymerUnreacted)
 
     ## Part 1 ##
     polymerReacted = applyReactions(polymerUnreacted)
-    print('The un-reacted polymer has a length of {}.'.format(polymerUnreactedLength))
-    print('The fully-reacted polymer has a length of {} after {} reactions.\n'.format(len(polymerReacted), reactions))
-    
+    print('\nThe un-reacted polymer has a length of {}.'.format(polymerUnreactedLength))
+    print('The fully-reacted polymer has a length of {}.\n'.format(len(polymerReacted)))
+
     ## Part 2 ##
     # Check the fully-reacted length of all polymers with one unit type (letter) subtracted
     polymerReducedLengths = {}
     for badUnit in list(string.ascii_lowercase):
 
-        polymerReduced = polymerUnreacted
+        polymerReduced = polymerUnreacted.copy()
         for idx in range(len(polymerReduced))[::-1]:  # step through all indices of polymer list backwards
             if polymerReduced[idx].lower() == badUnit:
                 polymerReduced.pop(idx)  # remove any units with the current bad unit type (starts at end of list to avoid index misalignment)
@@ -53,4 +56,4 @@ if __name__ == "__main__":
         print('Removing all {}/{} units, the fully-reacted polymer has a length of {}.'.format(badUnit, badUnit.upper(), len(polymerReducedReacted)))
 
     shortestReducedPolymer = list(filter(lambda x:x[1] == min(polymerReducedLengths.values()), polymerReducedLengths.items()))[0]
-    print('\n')
+    print('\nThe shortest reduced polymer has length {}.\n'.format(shortestReducedPolymer))
